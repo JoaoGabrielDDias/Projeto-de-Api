bd59289cdb9022e9df198c2c66da9bbaf6509d2c
joao gabriel <62572607+JoaoGabrielDDias@users.noreply.github.com>
2024-10-04 10:22:17 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,69 +1,43 @@
-const express = require('express')
+import {getPessoa, createPessoa, getPessoas} from './repositories/pessoas.js'
+import express from 'express'
 const app = express()
 
-const pg = require('pg')
+import pg from 'pg'
 const { Client } = pg
 const client = new Client({
-    user: 'postgres',
-    password: 'batatinha',
-    host: 'localhost',
-    port: 5432,
-  });
+  user: 'postgres',
+  password: 'batatinha',
+  host: 'localhost',
+  port: 5432,
+});
 
 client.connect()
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-const pessoas = [{
-    nome: 'joao',
-    idade: 22,
-    email: 'jgddminecraft12@gmail.com',
-    nickjogo: 'moviments'
-},{
-    nome: 'joao',
-    idade: 22,
-    email: 'jgddminecraft12@gmail.com',
-    nickjogo: 'moviments'
-},]
-app.get('/pessoas' , function (req, res){
-    
-    res.json(pessoas)
-})
-  app.get('/pessoa' , function (req, res){
 
-
-    /* client.query('SELECT * FROM pessoas',(error, dbres) => {
-        res.json(dbres.rows)
-       
-      }); */  //retorna tabela completa do banco
-
-    const index = 53
-
-    client.query('SELECT * FROM pessoas WHERE id = $1', [index], (error, dbres) => {
-        res.json(dbres.rows[0])
-        
-      });
-
-  });
-
-app.post('/pessoa', function(req, res){
-    console.log(req.body)
-    client.query('insert into pessoas(nome, email, idade, nickname) values ($1, $2, $3, $4) RETURNING *',
-    [req.body.nome, req.body.email, req.body.idade, req.body.nickname], (error, dbres)=> {
-        if(error){
-           res.json({error: error}) 
-           return
-        }
-        res.json(dbres.rows[0])
-    }    
-    )
+app.get('/pessoas', async function (req, res) {
+	const pessoas = await getPessoas(client)  
+	res.json(pessoas)
 });
 
+app.get('/pessoa/:id', async function (req, res) {
+  const index = req.params.id
+  const pessoa = await getPessoa(index, client)
+  res.json(pessoa)
+});
 
-app.patch('/pessoa/aniversario', (req, res)=>{
-    for(let batatinha = 0; batatinha < pessoas.length; batatinha++){
-        pessoas[batatinha].idade = pessoas[batatinha].idade + 1
-    }
-    res.json(pessoas)
+app.post('/pessoa', async function (req, res) {
+  console.log(req.body)
+  const result = await createPessoa(req.body, client)
+  res.json(result)
+});
+
+app.patch('/pessoa/aniversario', (_, res) => {
+  for (let batatinha = 0; batatinha < pessoas.length; batatinha++) {
+    pessoas[batatinha].idade = pessoas[batatinha].idade + 1
+  }
+  res.json(pessoas)
 });
 app.listen(3000)
+console.log('Api Ativa')
---
+++ repositories/pessoas.js
@@ -0,0 +1,16 @@
+
+export const getPessoa = async (id, client) => {
+    const result = await client.query('SELECT * FROM pessoas WHERE id = $1', [id]);
+    const pessoa = result.rows[0]
+    return pessoa
+}
+export const createPessoa = async (pessoa, client) => {
+    const res = await client.query('insert into pessoas(nome, email, idade, nickname) values ($1, $2, $3, $4) RETURNING *',
+    [pessoa.nome, pessoa.email, pessoa.idade, pessoa.nickname])
+	return res.rows[0]
+}
+export const getPessoas = async (client) => {
+	const result = await client.query('SELECT * FROM pessoas');
+	const pessoas = result.rows
+	return pessoas
+}
